b39e9feda13945a5a87fe7733e3b5af6e5d7d6ff
danyl borysenko <danylborysenko@gmail.com>
2025-11-26 18:15:46 +0100

    ft_itoa_unsd_int function was created

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: danborys <borysenkodanyl@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:50:51 by danborys          #+#    #+#             */
-/*   Updated: 2025/11/25 14:11:30 by danborys         ###   ########.fr       */
+/*   Updated: 2025/11/26 18:07:30 by danborys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,39 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+char	*ft_itoa_unsd_int(unsigned int i)
+{
+	unsigned int	nb;
+	int				chars_count;
+	char			*ptr;
+
+	nb = i;
+	chars_count = 1;
+	while (nb >= 10)
+	{
+		chars_count++;
+		nb = nb / 10;
+	}
+	ptr = malloc(sizeof(char) * (chars_count + 1));
+	if (!ptr)
+		return (NULL);
+	ptr[chars_count] = '\0';
+	chars_count--;
+	while (chars_count >= 0)
+	{
+		ptr[chars_count] = (i % 10) + '0';
+		i = i / 10;
+		chars_count--;
+	}
+	return (ptr);
+}
+
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
 	int	count;
 	char *str_arg;
-	
+
 	count = 0;
 	va_start(args, str);
 	while (*str)
@@ -32,10 +59,17 @@ int	ft_printf(const char *str, ...)
 				ft_putchar_fd('%', 1);
 				count++;
 			}
+			else if (*str == 'u')
+			{
+				str_arg = ft_itoa_unsd_int(va_arg(args, unsigned int));
+				count += ft_strlen(str_arg);
+				ft_putstr_fd(str_arg, 1);
+				free(str_arg);
+			}
 			else if (*str == 'c')
 			{
 				ft_putchar_fd(va_arg(args, int), 1);
-				count++;				
+				count++;
 			}
 			else if (*str == 's')
 			{
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int	ft_printf(const char *, ...);
+char	*ft_itoa_unsd_int(unsigned int i);
 
 int	main(void)
 {
-	ft_printf("%d\n", 052);
-	ft_printf("%i\n", 052);
-	ft_printf("%%\n");
+	int expeacted = printf("%u\n", UINT_MAX);
+	int actual = ft_printf("%u\n", UINT_MAX);
+	printf("expected : %d, actual : %d\n", expeacted, actual);
 }
